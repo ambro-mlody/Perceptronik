160c62f9a72d740f4e3fd9a7b444531b448ac1c6
ambro-mlody <52577549+ambro-mlody@users.noreply.github.com>
2021-10-22 21:14:50 +0200

    button

--- button.py
+++ button.py
@@ -1,12 +1,20 @@
 import pygame as py
 
+
 class Button(object):
 
-    def __init__(self, size, position, text, color, bg_color):
-        self.size = size
+    def __init__(self, position, size, text, font_size=12, color="white", bg_color="black"):
         self.position = position
         self.text = text
         self.color = color
-        self.bg_color = color
+        self.bg_color = bg_color
+        self.font = py.font.SysFont("Arial", font_size)
+        self.text_render = self.font.render(text, 1, color)
+        x, y, w, h = self.text_render.get_rect()
+        self.rect = py.Rect(position, size)
+        self.text_pos = (position[0] + (size[0] - w) / 2, position[1] + (size[1] - h) / 2)
+        print(self.text_pos)
 
     def draw(self, window):
+        py.draw.rect(window, self.bg_color, self.rect)
+        window.blit(self.text_render, self.text_pos)
--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import sys
 import pygame as py
+import button
 
 py.init()
 size = (800, 600)
 window = py.display.set_mode(size)
 py.display.set_caption("Perceptronik")
+b = button.Button((0, 0), (40, 40), "7", 34, bg_color="blue")
 
 while True:
     window.fill((0, 0, 0))
@@ -12,3 +14,6 @@ while True:
         if ev.type == py.QUIT:
             py.quit()
             sys.exit(0)
+
+    b.draw(window)
+    py.display.update()
